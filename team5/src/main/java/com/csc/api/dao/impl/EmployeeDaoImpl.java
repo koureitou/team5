e17424ca174bf0ab043b71def30356992484bf04
koureitou <kou.reitou@casc.co.jp>
2023-12-13 15:50:23 +0900

    update

--- team5/src/main/java/com/csc/api/dao/impl/EmployeeDaoImpl.java
+++ team5/src/main/java/com/csc/api/dao/impl/EmployeeDaoImpl.java
@@ -2,6 +2,7 @@ package com.csc.api.dao.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,6 +14,7 @@ import com.csc.api.entity.Employee;
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
 	
+	@Autowired
 	private JdbcTemplate jdbctemplate;
 
 	@Override
