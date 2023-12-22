f5bc5b54065d97ea577e09e2c4245895bbad5283
koureitou <kou.reitou@casc.co.jp>
2023-12-22 17:17:00 +0900

    update

--- team5/src/main/java/com/csc/api/control/LoginController.java
+++ team5/src/main/java/com/csc/api/control/LoginController.java
@@ -3,12 +3,17 @@ package com.csc.api.control;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.csc.api.entity.User;
+import com.csc.api.form.LoginForm;
 import com.csc.api.service.UseService;
+
+import jakarta.validation.constraints.NotBlank;
 
 @Controller
 public class LoginController {
@@ -16,18 +21,25 @@ public class LoginController {
     @Autowired
 	private UseService useService;
 	@GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(@Validated LoginForm loginForm) {
         return "login";
     }
 
     @PostMapping("/findByUsername")
-    public String login(@RequestParam String userName, @RequestParam String pwd, Model model) {
-        User user = useService.findByUsername(userName);
-        if (user != null && user.getPwd().equals(pwd)) {
-            return "redirect:/findAll";
-        } else {
-            model.addAttribute("error", "ユーザー名前またパスワードは間違いです、もう一度を確認してください！");
-            return "login";
-        }
-    }
+    public String login(@Validated LoginForm loginForm,BindingResult result, Model model) {
+        User user = useService.findByUsername(loginForm.getUserName());
+        
+        if(result.hasErrors()) {
+        
+      	   return "login";
+         }else {
+        	 if (user != null && user.getPwd().equals(loginForm.getPwd())) {
+                 return "redirect:/findAll";
+             } else {
+                 model.addAttribute("error", "ユーザー名前またパスワードは間違いです、もう一度を確認してください！");
+                 return "login";
+             }
+         }
+        
+    } 
 }
--- team5/src/main/java/com/csc/api/form/LoginForm.java
+++ team5/src/main/java/com/csc/api/form/LoginForm.java
@@ -2,6 +2,7 @@ package com.csc.api.form;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -12,7 +13,7 @@ public class LoginForm implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	
+	@NotBlank(message = "※名前漢字を入力してください")
 	private String userName;
 	private String pwd;
 	
